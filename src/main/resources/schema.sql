bf57fdd98fa5a917011009ff838b75f02b85c661
Abbas Hamad <abbashamad@Abbass-MacBook-Air.local>
2026-04-22 22:28:52 +0200

    Fiks igen da alt bliver ved med at crashe

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,28 +1,25 @@
-CREATE TABLE Users
-(
-    user_id  INT AUTO_INCREMENT PRIMARY KEY,
-    name     VARCHAR(255),
-    email    VARCHAR(255) UNIQUE,
-    password VARCHAR(255)
+CREATE TABLE IF NOT EXISTS Users (
+                                     user_id INT AUTO_INCREMENT PRIMARY KEY,
+                                     name VARCHAR(255),
+                                     email VARCHAR(255) UNIQUE,
+                                     password VARCHAR(255)
 );
 
-CREATE TABLE Wishlist
-(
-    wishlist_id INT AUTO_INCREMENT PRIMARY KEY,
-    user_id     INT,
-    name        VARCHAR(255),
-    FOREIGN KEY (user_id) REFERENCES Users (user_id)
+CREATE TABLE IF NOT EXISTS WishList (
+                                        wishlist_id INT AUTO_INCREMENT PRIMARY KEY,
+                                        user_id INT,
+                                        name VARCHAR(255),
+                                        FOREIGN KEY (user_id) REFERENCES Users(user_id)
 );
 
-CREATE TABLE Wish
-(
-    wish_id     INT AUTO_INCREMENT PRIMARY KEY,
-    wishlist_id INT          NOT NULL,
-    title       VARCHAR(255) NOT NULL,
-    description TEXT,
-    price       DECIMAL(15, 2),
-    link        VARCHAR(1000),
-    is_bought   BOOLEAN,
-    FOREIGN KEY (wishlist_id) REFERENCES Wishlist (wishlist_id)
-        ON DELETE CASCADE
+CREATE TABLE IF NOT EXISTS Wish (
+                                    wish_id INT AUTO_INCREMENT PRIMARY KEY,
+                                    wishlist_id INT NOT NULL,
+                                    title VARCHAR(255) NOT NULL,
+                                    description TEXT,
+                                    price DECIMAL(15, 2),
+                                    link VARCHAR(1000),
+                                    is_bought BOOLEAN,
+                                    FOREIGN KEY (wishlist_id) REFERENCES WishList(wishlist_id)
+                                        ON DELETE CASCADE
 );
